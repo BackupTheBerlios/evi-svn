e307001384312f9eecea77c59daf5f8af9dcb42d
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-05-18 22:42:10 +0000

    Testmodule now shows table with modules and the number of instances.

--- framework/src/org/schwering/evi/test/Test.java
+++ framework/src/org/schwering/evi/test/Test.java
@@ -1,36 +1,54 @@
 package org.schwering.evi.test;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+import org.schwering.evi.conf.MainConfiguration;
 import org.schwering.evi.core.*;
-import org.schwering.evi.util.HTMLBrowser;
 
 /**
  * Test module.
  * @author Christoph Schwering (mailto:schwering@gmail.com)
  * @version $Id: Test.java 50 2006-05-11 00:21:14Z schwering $
  */
-public class Test extends HTMLBrowser implements IModule, IPanel {
+public class Test extends JPanel implements IModule, IPanel {
 	private static final long serialVersionUID = 6675184535258501432L;
 	
-	public Test(URL url) {
-		super(url.toString());
-		System.out.println("module starts by command line argument: '"+ url +"'");
-	}
-
-	public Test(String url) {
-		super(url);
-		System.out.println("module starts with args: '"+ url +"'");
+	public Test() {
+		super(new BorderLayout());
+		String[] ids = ModuleLoader.getLoadedIds();
+		Object[][] data = new Object[ids.length][2];
+		for (int i = 0; i < data.length; i++) {
+			data[i][0] = ids[i];
+			data[i][1] = String.valueOf(ModuleLoader.getLoadedModule(ids[i]).getInstances().length);
+		}
+		JTable table = new JTable(data, new String[] { "ID", "Instances" });
+		
+		JLabel label = new JLabel("Running Modules");
+		label.setForeground(MainConfiguration.getColor("color.primary"));
+		label.setBackground(MainConfiguration.getColor("color.secondary"));
+		label.setFont(MainConfiguration.getFont("font.primary"));
+		
+		add(label, BorderLayout.NORTH);
+		add(table, BorderLayout.CENTER);
 	}
 	
-	public Test() {
-		super("hello.html");
-		System.out.println("module starts without args");
+	public Icon getIcon() {
+		return null;
+	}
+	
+	public Component getPanelInstance() {
+		return this;
 	}
 	
 	public void dispose() {
 		System.out.println("IModule.dispose() in org.schwering.evi.test.Test");
-		super.dispose();
 	}
 	
 	/* (non-Javadoc)
