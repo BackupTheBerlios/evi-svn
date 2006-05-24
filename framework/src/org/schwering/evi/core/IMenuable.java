329fedd33e47737e81737f3e8a3c2dc1e6f16f81
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-05-24 20:09:03 +0000

    Updated javadocs.

--- framework/src/org/schwering/evi/core/IMenuable.java
+++ framework/src/org/schwering/evi/core/IMenuable.java
@@ -1,8 +1,10 @@
 package org.schwering.evi.core;
 
 /**
- * Interface for modules that have menu, see 
- * {@link IDefaultMenuable} and {@link ICustomMenuable}.
+ * Interface for modules that have menu.<br />
+ * Implementing this interface has no effect, at least no positive (but an 
+ * exception possibly). Instead, you might want to implement one of its 
+ * subinterfaces: {@link IDefaultMenuable} and {@link ICustomMenuable}.
  * @see ModuleMenuInvoker
  * @see ModuleContainer#isMenuable()
  * @author Christoph Schwering (mailto:schwering@gmail.com)
