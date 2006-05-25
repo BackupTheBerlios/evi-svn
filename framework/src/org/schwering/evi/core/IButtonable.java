85058f23ff8e8ba5f0c8174d1aa8001a89924a65
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-05-25 01:55:13 +0000

    Added IButtonable, first attempt of given the modules control over having a button.

---
+++ framework/src/org/schwering/evi/core/IButtonable.java
@@ -0,0 +1,13 @@
+package org.schwering.evi.core;
+
+/**
+ * Interface for modules display a button in the toolbar.<br />
+ * <br />
+ * Modules that implement this interface will have a quick start 
+ * button at the toolbar.
+ * @see ModuleContainer#isButtonable()
+ * @author Christoph Schwering (mailto:schwering@gmail.com)
+ * @version $Id$
+ */
+public interface IButtonable extends IModule {
+}
