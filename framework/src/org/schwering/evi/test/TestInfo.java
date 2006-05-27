4d83ff549574717b4fd0c5a828037c7befecf98d
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-05-27 14:05:31 +0000

    Module is now parameterizable.

--- framework/src/org/schwering/evi/test/TestInfo.java
+++ framework/src/org/schwering/evi/test/TestInfo.java
@@ -3,8 +3,17 @@ package org.schwering.evi.test;
 import org.schwering.evi.core.IButtonable;
 import org.schwering.evi.core.IModule;
 import org.schwering.evi.core.IModuleInfo;
+import org.schwering.evi.core.IParameterizable;
 
-public class TestInfo implements IModuleInfo, IButtonable {
+public class TestInfo implements IModuleInfo, IButtonable, IParameterizable {
+
+	public IModule newInstance() {
+		return new Test();
+	}
+	
+	public IModule newInstance(Object[] args) {
+		return new Test((String)args[0]);
+	}
 
 	public String getInfoURL() {
 		return "about.html";
@@ -20,10 +29,6 @@ public class TestInfo implements IModuleInfo, IButtonable {
 
 	public float getVersion() {
 		return 0;
-	}
-
-	public IModule newInstance() {
-		return new Test();
 	}
 
 	public boolean isButtonable() {
