7291f15eea81e114c6cb3fef3799768b1b24c5e2
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-06-03 03:06:08 +0000

    Improved contents.

--- homepage/upload.sh
+++ homepage/upload.sh
@@ -1,9 +1,9 @@
 iconv --from=utf-8 --to=iso-8859-1 index.copl.de | ./copli --nohttp >index.html.de \
 && iconv --from=utf-8 --to=iso-8859-1 download.copl.de | ./copli --nohttp >download.html.de \
-&& iconv --from=utf-8 --to=iso-8859-1 resources.copl.de | ./copli --nohttp >resources.html.de \
+&& iconv --from=utf-8 --to=iso-8859-1 development.copl.de | ./copli --nohttp >development.html.de \
 && ./copli --nohttp index.copl.en index.html.en \
-&& ./copli --nohttp resources.copl.en resources.html.en \
 && ./copli --nohttp download.copl.en download.html.en \
+&& ./copli --nohttp development.copl.en development.html.en \
 && tar jcvf homepage.tar.bz2 --exclude=*.svn* --exclude=copli --exclude=homepage.tar.bz2 --exclude=*upload.sh* . \
 && scp -r homepage.tar.bz2 schwering@shell.berlios.de:/home/groups/evi/htdocs
 rm homepage.tar.bz2 \
