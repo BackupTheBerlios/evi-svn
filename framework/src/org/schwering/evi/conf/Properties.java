6f1b062100b2631b9ae796cad5790a5287e76a46
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-06-15 15:36:57 +0000

    Fixed: Default font.

--- framework/src/org/schwering/evi/conf/Properties.java
+++ framework/src/org/schwering/evi/conf/Properties.java
@@ -415,7 +415,7 @@ public class Properties extends java.util.Properties {
 	 * @return A new font.
 	 */
 	public Font getFont(String key) {
-		return getFont(key, Font.decode(null));
+		return getFont(key, new Font("SansSerif", 12, Font.PLAIN));
 	}
 	
 	/**
