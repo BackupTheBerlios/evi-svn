8246fc97afce4b3fff8f29e6008f7f0a945591e0
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-08-10 16:53:45 +0000

    Removed useless import.

--- framework/src/org/schwering/evi/conf/Properties.java
+++ framework/src/org/schwering/evi/conf/Properties.java
@@ -9,7 +9,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Enumeration;
 import java.util.Locale;
 
