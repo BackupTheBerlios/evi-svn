ce538d18507d98fc64d37ae42d95741f9c44c61a
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-09-22 04:19:10 +0000

    Removed useless import.

--- framework/src/org/schwering/evi/gui/conf/MainConfigurationPanel.java
+++ framework/src/org/schwering/evi/gui/conf/MainConfigurationPanel.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
-import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
