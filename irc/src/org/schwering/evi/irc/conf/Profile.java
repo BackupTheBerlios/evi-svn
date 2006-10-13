93f40f0954710713e9e5f5676ad8385a84609c83
schwering <schwering@da4c37f6-dd12-0410-af57-a82759ad0685>
2006-10-13 12:46:40 +0000

    Added Profile interface.

---
+++ irc/src/org/schwering/evi/irc/conf/Profile.java
@@ -0,0 +1,66 @@
+/* Copyright (C) 2006 Christoph Schwering */
+package org.schwering.evi.irc.conf;
+
+import java.awt.Font;
+import java.awt.Color;
+
+/**
+ * Interface for different profile types, file (= full) and URI profile.
+ * @author Christoph Schwering (mailto:schwering@gmail.com)
+ * @version $Id$
+ */
+public interface Profile {
+	public String getName();
+	
+	public String getServer();
+	
+	public int getPort();
+	
+	public boolean getSSL();
+	
+	public String getPassword();
+	
+	public String getNickname();
+	
+	public String getUsername();
+	
+	public String getRealname();
+	
+	public String getQuitMessage();
+	
+	public String getPartMessage();
+	
+	public Font getConsoleFont();
+	
+	public Font getChannelFont();
+	
+	public Font getQueryFont();
+	
+	public Color getOwnColor();
+	
+	public Color getOtherColor();
+	
+	public Color getNeutralColor();
+	
+	public boolean getEnableColors();
+	
+	public Color[] getColorPalette();
+	
+	public boolean getReconnect();
+	
+	public boolean getRejoinOnKick();
+		
+	public boolean getBeepOnMention();
+	
+	public boolean getBeepOnQuery();
+	
+	public boolean getEnableLogging();
+	
+	public String getLoggingDir();
+	
+	public boolean getAcceptCerts();
+	
+	public String getBrowser();
+	
+	public String getPerform();
+}
